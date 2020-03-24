d03fe1ba1feede9c4cf88d05ba7f81b51ff2bf42
Worapol Krichmaitri <worapol.54.31@gmail.com>
2020-03-24 23:27:15 +0700

    tester

--- mongodb/mongo.test.js
+++ mongodb/mongo.test.js
@@ -11,22 +11,22 @@ describe('createUser',function(){
                 console.log(response)
             })
 
-        // var data = { Email: "e2etest@hotmail.com" };
+        var data = { Email: "e2etest@hotmail.com" };
 
-        // // let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), '123456').toString();
-        // // let sending_data = {data: ciphertext};
+        // let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), '123456').toString();
+        // let sending_data = {data: ciphertext};
 
-        // axios("/job/addemployee/" + "173", {
-        //     method: 'PUT',
-        //     headers: { 'Content-Type': 'application/json' },
-        //     body: JSON.stringify(data)
-        // }).then(function (response) {
-        //     if (response.status >= 400) {
-        //     throw new Error("Bad response from server");
-        //     }
+        axios("/job/addemployee/" + "173", {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        }).then(function (response) {
+            if (response.status >= 400) {
+            throw new Error("Bad response from server");
+            }
         
-        //     // console.log(response);
-        // })
+            // console.log(response);
+        })
 
 
     })
