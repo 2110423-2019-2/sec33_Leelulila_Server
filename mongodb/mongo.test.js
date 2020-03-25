3e1bccdee62cd5eea51caa5c55ba00e03e578611
drive087 <d.thus_sk135@hotmail.com>
2020-03-25 22:02:40 +0700

    unittest dai ra ja

--- mongodb/mongo.test.js
+++ mongodb/mongo.test.js
@@ -1,28 +1,30 @@
 const assert = require('chai').assert;
 const CryptoJS = require('crypto-js');
 const fetch = require("node-fetch");
+const expect = require('chai').expect;
 
 describe('Apply Job',function(){
-    it('Response Success (200)',function(){
+    it('Response Success (200)',async () =>{
     
-        var email = "e2etest@hotmail.com";
+        var email = "onDemand@hotmail.com";
         var data = { Email: email};
     
         let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), '123456').toString();
         let sending_data = {data: ciphertext};
+       
 
-        fetch("http://localhost:9000/job/addemployee/173", {
+        await fetch("http://localhost:9000/job/addemployee/173", {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(sending_data)
-        }).then(function (response) {
-            if (response.status >= 400) {
-            throw new Error("Bad response from server");
-            }
-        
-            // console.log(response);
-            assert.equal(response.status,200);
+        }).then((res) => {
+            return res.json();
+        })
+        .then((res) => {
 
+            console.log('///////////////')
+            console.log(res);
+            assert.equal(res, 'No user with the email onDemand@hotmail.com');
         })
     })
 })
