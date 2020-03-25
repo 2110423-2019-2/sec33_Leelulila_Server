c639ca1b18e0cd46905b77ae464fc3f7731c83c3
Worapol Krichmaitri <worapol.54.31@gmail.com>
2020-03-25 20:32:43 +0700

    unit test create job (incomplete)

--- mongodb/mongo.test.js
+++ mongodb/mongo.test.js
@@ -2,6 +2,8 @@ const assert = require('chai').assert;
 const CryptoJS = require('crypto-js');
 const fetch = require("node-fetch");
 
+const expect = require('chai').expect;
+
 describe('Apply Job',function(){
     it('Response Success (200)',function(){
     
@@ -24,5 +26,38 @@ describe('Apply Job',function(){
             assert.equal(response.status,200);
 
         })
+    })
+})
+
+describe('Create Job', function(){
+    it('Response Success (200)', function(){
+    
+        var id = 123;
+    
+        // let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), '123456').toString();
+        // let sending_data = {id: ciphertext};
+
+        fetch("http://localhost:9000/job/173", {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            // body: JSON.stringify(id)
+
+        }).then(function (response) {
+            if (response.status <= 400) {
+                console.log("OK")
+                expect(response.status).to.equal(200)
+            // throw new Error("Bad response from server");
+            } else {
+                console.log("WRONG")
+                expect(response.status).to.equal(401)
+            }
+            
+        
+            console.log(response.status);
+            // assert.equal(response.status, 202);
+            // expect(response.status).toEqual(401)
+        })
+
+        // expect(response.status).toEqual(401)
     })
 })
